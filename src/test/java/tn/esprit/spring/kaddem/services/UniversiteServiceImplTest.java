de9f770e76ae64f01e252bd016cdd572a6781523
Ed Kalai <edkalai28@gmail.com>
2023-11-06 17:25:08 +0100

    added JUnit dependencies & implemented UniversiteService test class

---
+++ src/test/java/tn/esprit/spring/kaddem/services/UniversiteServiceImplTest.java
@@ -0,0 +1,115 @@
+package tn.esprit.spring.kaddem.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import tn.esprit.spring.kaddem.entities.Departement;
+import tn.esprit.spring.kaddem.entities.Universite;
+import tn.esprit.spring.kaddem.repositories.DepartementRepository;
+import tn.esprit.spring.kaddem.repositories.UniversiteRepository;
+
+import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+public class UniversiteServiceImplTest {
+
+    @Mock
+    private UniversiteRepository universiteRepository;
+
+    @Mock
+    private DepartementRepository departementRepository;
+
+    @InjectMocks
+    private UniversiteServiceImpl universiteService;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    public void testRetrieveAllUniversites() {
+        List<Universite> universites = new ArrayList<>();
+        when(universiteRepository.findAll()).thenReturn(universites);
+
+        List<Universite> result = universiteService.retrieveAllUniversites();
+
+        assertEquals(universites, result);
+    }
+
+    @Test
+    public void testAddUniversite() {
+
+        Universite universite = new Universite();
+        when(universiteRepository.save(universite)).thenReturn(universite);
+
+        Universite result = universiteService.addUniversite(universite);
+
+        assertEquals(universite, result);
+    }
+
+    @Test
+    public void testUpdateUniversite() {
+        Universite universite = new Universite();
+        when(universiteRepository.save(universite)).thenReturn(universite);
+
+        Universite result = universiteService.updateUniversite(universite);
+
+        assertEquals(universite, result);
+    }
+
+    @Test
+    public void testRetrieveUniversite() {
+        Universite universite = new Universite();
+        when(universiteRepository.findById(1)).thenReturn(Optional.of(universite));
+
+        Universite result = universiteService.retrieveUniversite(1);
+
+        assertEquals(universite, result);
+    }
+
+    @Test
+    public void testDeleteUniversite() {
+
+        Universite universite = new Universite();
+        when(universiteRepository.findById(1)).thenReturn(Optional.of(universite));
+
+        universiteService.deleteUniversite(1);
+
+        verify(universiteRepository, times(1)).delete(universite);
+    }
+
+    @Test
+    public void testAssignUniversiteToDepartement() {
+
+        Universite universite = new Universite();
+        Departement departement = new Departement();
+        when(universiteRepository.findById(1)).thenReturn(Optional.of(universite));
+        when(departementRepository.findById(1)).thenReturn(Optional.of(departement));
+
+        universiteService.assignUniversiteToDepartement(1, 1);
+
+        assertTrue(universite.getDepartements().contains(departement));
+        verify(universiteRepository, times(1)).save(universite);
+    }
+
+    @Test
+    public void testRetrieveDepartementsByUniversite() {
+
+        Universite universite = new Universite();
+        Departement departement = new Departement();
+        universite.getDepartements().add(departement);
+        when(universiteRepository.findById(1)).thenReturn(Optional.of(universite));
+
+        Set<Departement> result = universiteService.retrieveDepartementsByUniversite(1);
+
+        assertTrue(result.contains(departement));
+    }
+}
